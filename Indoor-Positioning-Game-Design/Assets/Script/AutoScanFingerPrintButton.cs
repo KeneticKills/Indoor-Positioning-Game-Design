6a6d415d14381ac02d25911a89dba0ebf6c8522a
SleepyO <s6501012610050@email.kmutnb.ac.th>
2025-09-26 17:34:32 +0700

    Menu Progress 01
    Very minor Update
    - new "Position scan" menu (still no function)

--- Indoor-Positioning-Game-Design/Assets/Script/AutoScanFingerPrintButton.cs
+++ Indoor-Positioning-Game-Design/Assets/Script/AutoScanFingerPrintButton.cs
@@ -3,6 +3,6 @@ using UnityEngine;
 public class AutoScanFingerPrintButton
 {
     public void fingerPrintAutoScan(){
-        //run FingerPrint Scan every 2 secs function here
+        Debug.Log("OwO");
     }
 }
