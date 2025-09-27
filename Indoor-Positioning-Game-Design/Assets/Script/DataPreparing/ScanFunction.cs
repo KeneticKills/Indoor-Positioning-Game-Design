405380097cae33bd49fc93eb3baaf1c894f9ffe4
KeneticKills <109983145+KeneticKills@users.noreply.github.com>
2025-09-27 18:28:53 +0700

    Input Text Box Functional
    - Fix textbox to accept number only
    - Loaded text and debug it

--- Indoor-Positioning-Game-Design/Assets/Script/DataPreparing/ScanFunction.cs
+++ Indoor-Positioning-Game-Design/Assets/Script/DataPreparing/ScanFunction.cs
@@ -1,10 +1,16 @@
 using UnityEngine;
 using System.Collections.Generic;
 using System.Collections;
+using TMPro;
 
 public class ScanFunction : MonoBehaviour
 {
+    public Transform InputX;
+    public Transform InputZ;
+
     public void onClick() {
-        Debug.Log("Test ABCDEFG");
+        string inputX = InputX.GetComponent<TextMeshProUGUI>().text;
+        string inputZ = InputZ.GetComponent<TextMeshProUGUI>().text;
+        Debug.Log("Input X : " + inputX + " ; Input Z : " + inputZ);
     }
 }
